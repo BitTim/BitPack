542b0f72ac2fdb29352af832601d7c2b6ac83528
BitTim <tim.anhalt.2004@protonmail.com>
2021-11-22 10:46:37 +0100

    Added checks for existing packages/versions/types

--- Server/src/index.js
+++ Server/src/index.js
@@ -13,19 +13,76 @@ app.get("/packages", (req, res) => {
 
 app.get("/packages/:id", (req, res) => {
     const { id } = req.params;
+
+    if(!util.packageExists(packageList, id))
+    {
+        res.status(404).send("Package " + id + " not found");
+        return;
+    }
+
     res.status(200).send(loader.loadPackage(id));
 });
 
 app.get("/packages/:id/:version", (req, res) => {
     const { id, version } = req.params;
     
+    if(!util.packageExists(packageList, id))
+    {
+        res.status(404).send("Package " + id + " not found");
+        return;
+    }
+
+    if(!util.versionExists(id, version))
+    {
+        res.status(404).send("Package " + id + " does not contain the version " + version);
+        return;
+    }
+
     const versionData = loader.loadPackage(id).versions;
     res.status(200).send(versionData.filter((v) => v.version == version)[0]);
 });
 
-app.get("/packages/:id/:version/:type", (req, res) => {
+app.get("/packages/:id/:version/download", (req, res) => {
+    const { id, version } = req.params;
+    
+    if(!util.packageExists(packageList, id))
+    {
+        res.status(404).send("Package " + id + " not found");
+        return;
+    }
+
+    if(!util.versionExists(id, version))
+    {
+        res.status(404).send("Package " + id + " does not contain the version " + version);
+        return;
+    }
+
+    const versionData = loader.loadPackage(id).versions;
+    const typeData = versionData.filter((v) => v.version == version)[0].downloadTypes;
+    res.status(200).send(typeData);
+});
+
+app.get("/packages/:id/:version/download/:type", (req, res) => {
     const { id, version, type } = req.params;
     
+    if(!util.packageExists(packageList, id))
+    {
+        res.status(404).send("Package " + id + " not found");
+        return;
+    }
+
+    if(!util.versionExists(id, version))
+    {
+        res.status(404).send("Package " + id + " does not contain the version " + version);
+        return;
+    }
+
+    if(!util.typeExists(id, version, type))
+    {
+        res.status(404).send("Package " + id + "-" + version + " does not contain the download type " + type);
+        return;
+    }
+
     const filename = id + "-" + version + "_" + type + ".zip";
     const path = constants.packagePath + id + "/versions/" + version + "/" + filename;
     
--- Server/src/util.js
+++ Server/src/util.js
@@ -1,9 +1,45 @@
 const loader = require("./loader");
 
+// Initialize server
 exports.init = (express, app, loader) =>
 {
     app.use(express.json())
 
     var packagelist = loader.generatePackageList();
     return packagelist;
+}
+
+// Check if package exists
+exports.packageExists = (packageList, id) =>
+{
+    var found = false;
+
+    for(var channel in packageList)
+    {
+        var filtred = packageList[channel].filter(p => p.id == id);
+        if(filtred.length > 0)
+        {
+            found = true;
+            break;
+        }
+    }
+
+    return found;
+}
+
+// Check if version of package exists
+exports.versionExists = (id, version) =>
+{
+    const versionData = loader.loadPackage(id).versions;
+    if(versionData.filter((v) => v.version == version).length > 0) return true;
+    else return false;
+}
+
+// Check if download type of version exists
+exports.typeExists = (id, version, type) =>
+{
+    const versionData = loader.loadPackage(id).versions;
+    const fetchedVersion = versionData.filter((v) => v.version == version)[0];
+    if(fetchedVersion.downloadTypes.indexOf(type) != -1) return true;
+    else return false;
 }
